278b429828a068ce26895bcb20c6fc3504e95a00
Sigitas <kompiuteris@Sigitass-MBP.lan>
2020-08-27 16:45:10 +0300

    commit

--- templates/sidebar/sidebar.js
+++ templates/sidebar/sidebar.js
@@ -1,31 +1,31 @@
 let sidebarHTML = `
     <section>
-        <a href="#"><i class="fas fa fa-mail-reply" aria-hidden="true"></i>Back to home</a>
+        <a class="nav-link" href="#"><i class="fas fa fa-mail-reply" aria-hidden="true"></i>Back to home</a>
     </section>
     <section>
         <p class="title">ACCOUNT MANAGEMENT</p>
-        <a href="../account/subscriptions.html"><i class="fa fa-chart-bar" aria-hidden="true"></i>Active subscription very long text is here</a>
-        <a href="../account/org_logo.html"><i class="fas fa fa-image" aria-hidden="true"></i>Organisation logo</a>
-        <a href="../account/org_details.html"><i class="fas fa fa-house" aria-hidden="true"></i>Organisation details</a>
-        <a href="../account/contacts.html"><i class="fas fa fa-phone" aria-hidden="true"></i>Contacts details</a>
-        <a href="../account/users.html"><i class="fas fa fa-user" aria-hidden="true"></i>User management</a>
-        <a href="../account/invoices.html"><i class="fas fa fa-file-invoice" aria-hidden="true"></i>Invoices</a>
+        <a class="nav-link" href="../account/subscriptions.html"><i class="fa fa-chart-bar" aria-hidden="true"></i>Active subscription very long text is here</a>
+        <a class="nav-link" href="../account/org_logo.html"><i class="fas fa fa-image" aria-hidden="true"></i>Organisation logo</a>
+        <a class="nav-link" href="../account/org_details.html"><i class="fas fa fa-house" aria-hidden="true"></i>Organisation details</a>
+        <a class="nav-link" href="../account/contacts.html"><i class="fas fa fa-phone" aria-hidden="true"></i>Contacts details</a>
+        <a class="nav-link" href="../account/users.html"><i class="fas fa fa-user" aria-hidden="true"></i>User management</a>
+        <a class="nav-link" href="../account/invoices.html"><i class="fas fa fa-file-invoice" aria-hidden="true"></i>Invoices</a>
     </section>
     <section>
         <p class="title">CONTENT RETENTION</p>
-        <a href="../retention/space.html"><i class="fa fa-chart-bar" aria-hidden="true"></i>Space content</a>
-        <a href="../retention/chat.html"><i class="fa fa-chart-bar" aria-hidden="true"></i>Chat content</a>
+        <a class="nav-link" href="../retention/space.html"><i class="fa fa-chart-bar" aria-hidden="true"></i>Space content</a>
+        <a class="nav-link" href="../retention/chat.html"><i class="fa fa-chart-bar" aria-hidden="true"></i>Chat content</a>
     </section>
     <section>
         <p class="title">PERSONAL SETTINGS</p>
-        <a href="../personal/personal_details.html"><i class="fas fa fa-info-square" aria-hidden="true"></i>Personal details</a>
-        <a href="../personal/picture.html"><i class="fas fa fa-id-badge" aria-hidden="true"></i>Change picture</a>
-        <a href="../personal/verify_identity.html"><i class="far fa fa-badge-check active" aria-hidden="true"></i>Verify identity</a>
-        <a href="../personal/account_list.html"><i class="far fa fa-building" aria-hidden="true"></i>Accounts list</a>
-        <a href="../personal/language.html"><i class="far fa fa-globe" aria-hidden="true"></i>Language</a>
+        <a class="nav-link" href="../personal/personal_details.html"><i class="fas fa fa-info-square" aria-hidden="true"></i>Personal details</a>
+        <a class="nav-link" href="../personal/picture.html"><i class="fas fa fa-id-badge" aria-hidden="true"></i>Change picture</a>
+        <a class="nav-link" href="../personal/verify_identity.html"><i class="far fa fa-badge-check active" aria-hidden="true"></i>Verify identity</a>
+        <a class="nav-link" href="../personal/account_list.html"><i class="far fa fa-building" aria-hidden="true"></i>Accounts list</a>
+        <a class="nav-link" href="../personal/language.html"><i class="far fa fa-globe" aria-hidden="true"></i>Language</a>
     </section>
     <section>
-        <a href="#"><i class="far fa fa-user-headset" aria-hidden="true"></i>Support</a>
-        <a href="#"><i class="far fa fa-sign-out" aria-hidden="true"></i>Sign out</a>
+        <a class="nav-link" href="#"><i class="far fa fa-user-headset" aria-hidden="true"></i>Support</a>
+        <a class="nav-link" href="#"><i class="far fa fa-sign-out" aria-hidden="true"></i>Sign out</a>
     </section>
 `
